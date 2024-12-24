85e6a5fefe3e46846ffa1b2c90566b0e422e852f
Tudor Gradinaru <126348970+TudorGR@users.noreply.github.com>
2024-12-24 13:41:19 +0200

    Added routes and controllers for user and product

---
+++ backend/controllers/userController.js
@@ -0,0 +1,73 @@
+import userModel from "../models/userModel.js";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+
+//route for user login
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "User does not exists" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      const token = createToken(user._id);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Incorrect password" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//route for user signup
+const signupUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    //email already used
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+
+    //validating email format and password
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: "Email not correct" });
+    }
+    if (password.length < 8) {
+      return res.json({ success: false, message: "Password not strong" });
+    }
+
+    //hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+
+    const token = createToken(user._id);
+
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//rout for admin login
+const loginAdmin = async (req, res) => {};
+
+export { loginUser, signupUser, loginAdmin };
